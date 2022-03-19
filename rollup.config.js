43b831cc5eb4d6ad6492b7fe506413c857b097ce
Dmitry Bardyshev <brd.dmitry@gmail.com>
2022-03-19 13:56:28 +0300

    Minify bundle with `terser`

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import { terser } from "rollup-plugin-terser";
 import dts from "rollup-plugin-dts";
 
 const packageJson = require("./package.json");
@@ -26,6 +27,7 @@ export default [
 			resolve(),
 			commonjs(),
 			typescript({ tsconfig: "./tsconfig.json" }),
+			terser(),
 		],
 	},
 	{
