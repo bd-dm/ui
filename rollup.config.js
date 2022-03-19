9b6d2c078ab8a6c2bf4b4ac214064c8be0e457ba
Dmitry Bardyshev <brd.dmitry@gmail.com>
2022-03-19 14:36:35 +0300

    Added `autoprefixer`

--- rollup.config.js
+++ rollup.config.js
@@ -4,6 +4,7 @@ import typescript from "@rollup/plugin-typescript";
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import { terser } from "rollup-plugin-terser";
 import postcss from "rollup-plugin-postcss";
+import autoprefixer from "autoprefixer";
 import dts from "rollup-plugin-dts";
 
 const packageJson = require("./package.json");
@@ -29,6 +30,7 @@ export default [
 				extract: false,
 				modules: true,
 				use: ["sass"],
+				plugins: [autoprefixer],
 			}),
 			resolve(),
 			commonjs(),
