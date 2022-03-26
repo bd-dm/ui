aabff9f2cf7e31ef5266150b112cb33a36be79fd
Dmitry Bardyshev <brd.dmitry@gmail.com>
2022-03-26 19:21:19 +0300

    Fixed `useDeviceTheme` re-renders

--- src/hooks/useDeviceTheme.ts
+++ src/hooks/useDeviceTheme.ts
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { UITheme } from "types";
 
 const useDeviceTheme = () => {
@@ -8,11 +8,11 @@ const useDeviceTheme = () => {
 		setDeviceTheme(isDarkTheme ? UITheme.DARK : UITheme.LIGHT);
 	};
 
-	if (typeof window !== "undefined") {
+	useEffect(() => {
 		const isDarkTheme = window.matchMedia("(prefers-color-scheme: dark)");
 		isDarkTheme.onchange = (e) => onIsDarkThemeChange(e.matches);
 		onIsDarkThemeChange(isDarkTheme.matches);
-	}
+	});
 
 	return deviceTheme;
 };
