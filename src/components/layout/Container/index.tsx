6b97c06474aff6f19ad7f038dd9a111142d62ccb
Dmitry Bardyshev <brd.dmitry@gmail.com>
2022-03-29 21:59:20 +0300

    Added className prop to `Container`

--- src/components/layout/Container/index.tsx
+++ src/components/layout/Container/index.tsx
@@ -1,10 +1,12 @@
 import { FC } from "react";
 import { WithClassName } from "types";
-import styles from "./index.module.scss";
 import classNames from "classnames";
+import styles from "./index.module.scss";
 
 const Container: FC<WithClassName> = ({ children, className }) => {
-	return <div className={classNames(styles.container, className)}>{children}</div>;
+	return (
+		<div className={classNames(styles.container, className)}>{children}</div>
+	);
 };
 
 export { Container };
